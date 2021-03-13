fdd83eb43ac2dc55bcd45fca9488f746c3a87c09
Leidy J <44031927+Leidysalda@users.noreply.github.com>
2021-03-13 12:09:07 -0500

    Add files via upload

---
+++ Guide3_Java_data_structure/Guide3_Activity3/EvaluadorPostfijo/src/universidadean/desarrollosw/postfijo/EvaluadorPostfijo.java
@@ -0,0 +1,65 @@
+/**
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Universidad Ean (Bogotá - Colombia)
+ * Departamento de Tecnologías de la Información y Comunicaciones
+ * Licenciado bajo el esquema Academic Free License version 2.1
+ * <p>
+ * Proyecto Evaluador de Expresiones Postfijas
+ * Fecha: Febrero 2021
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+package universidadean.desarrollosw.postfijo;
+
+import java.util.*;
+
+/**
+ * Esta clase representa una clase que evalúa expresiones en notación polaca o
+ * postfija. Por ejemplo: 4 5 +
+ */
+public class EvaluadorPostfijo {
+
+    /**
+     * Realiza la evaluación de la expresión postfijo utilizando una pila
+     * @param expresion una lista de elementos con números u operadores
+     * @return el resultado de la evaluación de la expresión.
+     */
+    static int evaluarPostFija(List<String> tok) {
+        Stack<Integer> pila = new Stack<>();
+
+        for (String elem : tok) {
+            if (elem.equals(Token.TokenType.TOKEN_OPERATOR)) {
+
+            }
+            pila.push(Integer.parseInt(elem));
+            }
+
+        System.out.println("lista " + tok);
+        System.out.println("Pila " + pila);
+
+
+        List<String> operaccion = new LinkedList<>();
+
+        // TODO: Realiza la evaluación de la expresión en formato postfijo
+
+        return pila.pop();
+    }
+
+    /**
+     * Programa principal
+     */
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("> ");
+        String linea = teclado.nextLine();
+
+        try {
+            List<String> expresion = Token.dividir(linea);
+            System.out.println(evaluarPostFija(expresion));
+        }
+        catch (Exception e) {
+            System.err.printf("Error grave en la expresión: %s", e.getMessage());
+        }
+
+    }
+}
